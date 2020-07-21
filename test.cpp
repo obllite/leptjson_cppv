3f23f3bfb921293b0f4bb423b454e110ba393ed8
obllite <869416842@qq.com>
2020-07-21 23:58:01 +0800

    finished addition to json builder
    finished the test case of number, boolean, null, object, array and string

--- test.cpp
+++ test.cpp
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lept_context.h"
+#include "json-parser.h"
 using namespace::std;
 
 static int main_ret = 0;
 static int test_count = 0;
 static int test_pass = 0;
 
-// 宏函数，永允滴神。只用inline没有通用性，只用template没有效率，两者都用的inline template只在完全特化模板的情况下
-// 编译器才会接受inline建议，等于没用template。
 #define EXPECT_EQ_BASE(equality, expect, actual, format) \
     do {\
         test_count++;\
@@ -21,124 +19,192 @@ static int test_pass = 0;
     } while(0)
 
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
+
+static void testNull() {
+    Parser parser{ "null" };
+    JsonToken val = parser.parse();
+    EXPECT_EQ_INT(JsonType::kNull, val.type());
+}
+
+static void testTrue() {
+    Parser parser{ "true" };
+    JsonToken val = parser.parse();
+    EXPECT_EQ_INT(JsonType::kBool, val.type());
+}
+
+static void testFalse() {
+    Parser parser{ "true" };
+    JsonToken val = parser.parse();
+    EXPECT_EQ_INT(JsonType::kBool,val.type());
+}
+
+
 #define EXPECT_EQ_DOUBLE(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%.17g")
 
-#define TEST_NUMBER(expect, json)\
+inline void testNumber(double expect, const char* json)
+{
+    Parser parser{ json };
+    JsonToken macro_val = parser.parse();
+    EXPECT_EQ_INT(JsonType::kNumber, macro_val.type());
+    EXPECT_EQ_DOUBLE(expect, macro_val.toNumber());
+}
+
+static void testNumber() {
+    testNumber(0.0, "0");
+    testNumber(0.0, "-0");
+    testNumber(0.0, "-0.0");
+    testNumber(1.0, "1");
+    testNumber(-1.0, "-1");
+    testNumber(1.5, "1.5");
+    testNumber(-1.5, "-1.5");
+    testNumber(3.1416, "3.1416");
+    testNumber(1E10, "1E10");
+    testNumber(1e10, "1e10");
+    testNumber(1E+10, "1E+10");
+    testNumber(1E-10, "1E-10");
+    testNumber(-1E10, "-1E10");
+    testNumber(-1e10, "-1e10");
+    testNumber(-1E+10, "-1E+10");
+    testNumber(-1E-10, "-1E-10");
+    testNumber(1.234E+10, "1.234E+10");
+    testNumber(1.234E-10, "1.234E-10");
+    testNumber(0.0, "1e-10000000000000"); /* underflow */
+
+    testNumber(1.0000000000000002, "1.0000000000000002");             /* the smallest number > 1 */
+    testNumber(4.9406564584124654e-324, "4.9406564584124654e-324");   /* minimum denormal */
+    testNumber(-4.9406564584124654e-324,  "-4.9406564584124654e-324");
+    testNumber(2.2250738585072009e-308,  "2.2250738585072009e-308");  /* Max subnormal double */
+    testNumber(-2.2250738585072009e-308,  "-2.2250738585072009e-308");
+    testNumber(2.2250738585072014e-308,  "2.2250738585072014e-308");  /* Min normal positive double */
+    testNumber(-2.2250738585072014e-308,  "-2.2250738585072014e-308");
+    testNumber(1.7976931348623157e+308, "1.7976931348623157e+308");   /* Max double */
+    testNumber(-1.7976931348623157e+308,  "-1.7976931348623157e+308");
+}
+
+
+#define EXPECT_EQ_STRING(expect, actual, alength) \
+    EXPECT_EQ_BASE(sizeof(expect) - 1 == alength && memcmp(expect, actual, alength) == 0, expect, actual, "%s")
+
+#define TEST_STRING(expect, json)\
     do {\
-        lept_value macro_v;\
-        lept_context context{json};\
-        EXPECT_EQ_INT(LEPT_PARSE_OK, context.lept_parse(macro_v));\
-        EXPECT_EQ_INT(lept_type::LEPT_NUMBER, macro_v.lept_get_type());\
-        EXPECT_EQ_DOUBLE(expect ,macro_v.lept_get_num());\
+        Parser parser{ json };\
+        JsonToken val = parser.parse();\
+        EXPECT_EQ_INT(JsonType::kString, val.type());\
+        EXPECT_EQ_STRING(expect, val.toString().c_str(), val.toString().length());\
     } while(0)
 
-#define TEST_ERROR(error,  json)\
-    do {\
-        lept_value macro_v{lept_type::LEPT_FALSE, 0};\
-        lept_context context{json};\
-        EXPECT_EQ_INT(error, context.lept_parse(macro_v));\
-        EXPECT_EQ_INT(lept_type::LEPT_NULL, macro_v.lept_get_type());\
-    } while(0)
-
-static void test_parse_null() {
-    lept_value value{ lept_type::LEPT_FALSE, 0 };
-    lept_context context{ "null" };
-    EXPECT_EQ_INT((LEPT_PARSE_OK), context.lept_parse(value));
-    EXPECT_EQ_INT(lept_type::LEPT_NULL, value.lept_get_type());
+static void testString() {
+    TEST_STRING("", "\"\"");
+    TEST_STRING("Hello", "\"Hello\"");
+    TEST_STRING("Hello\nWorld", "\"Hello\\nWorld\"");
+    TEST_STRING("\" \\ / \b \f \n \r \t", "\"\\\" \\\\ \\/ \\b \\f \\n \\r \\t\"");
+    TEST_STRING("Hello\0World", "\"Hello\\u0000World\"");
+    TEST_STRING("\x24", "\"\\u0024\"");         /* Dollar sign U+0024 */
+    TEST_STRING("\xC2\xA2", "\"\\u00A2\"");     /* Cents sign U+00A2 */
+    TEST_STRING("\xE2\x82\xAC", "\"\\u20AC\""); /* Euro sign U+20AC */
+    TEST_STRING("\xF0\x9D\x84\x9E", "\"\\uD834\\uDD1E\"");  /* G clef sign U+1D11E */
+    TEST_STRING("\xF0\x9D\x84\x9E", "\"\\ud834\\udd1e\"");  /* G clef sign U+1D11E */
 }
 
-static void test_parse_true() {
-    lept_value value{ lept_type::LEPT_FALSE, 0 };
-    lept_context context{ "true" };
-    EXPECT_EQ_INT(LEPT_PARSE_OK, context.lept_parse(value));
-    EXPECT_EQ_INT(lept_type::LEPT_TRUE, value.lept_get_type());
+
+#if defined(_MSC_VER)
+#define EXPECT_EQ_SIZE_T(expect, actual) EXPECT_EQ_BASE((expect) == (actual), (size_t)expect, (size_t)actual, "%Iu")
+#else
+#define EXPECT_EQ_SIZE_T(expect, actual) EXPECT_EQ_BASE((expect) == (actual), (size_t)expect, (size_t)actual, "%zu")
+#endif
+
+static void testArray() {
+    Parser parser{ "[ ]" };
+    JsonToken val = parser.parse();
+
+    EXPECT_EQ_INT(JsonType::kArray, val.type());
+    EXPECT_EQ_SIZE_T(0, val.size());
+
+    parser.setContext("[ null , false , true , 123 , \"abc\" ]");
+    val = parser.parse();
+    EXPECT_EQ_INT(JsonType::kArray, val.type());
+    EXPECT_EQ_SIZE_T(5, val.size());
+    EXPECT_EQ_INT(JsonType::kNull, val[0].type());
+    EXPECT_EQ_INT(JsonType::kBool, val[1].type());
+    EXPECT_EQ_INT(JsonType::kBool, val[2].type());
+    EXPECT_EQ_INT(JsonType::kNumber, val[3].type());
+    EXPECT_EQ_INT(JsonType::kString, val[4].type());
+    EXPECT_EQ_DOUBLE(123.0, val[3].toNumber());
+    EXPECT_EQ_STRING("abc", val[4].toString().c_str(), val[4].toString().length());
+
+    parser.setContext("[ [ ] , [ 0 ] , [ 0 , 1 ] , [ 0 , 1 , 2 ] ]");
+    val = parser.parse();
+    EXPECT_EQ_INT(JsonType::kArray, val.type());
+    EXPECT_EQ_SIZE_T(4, val.toArray().size());
+    for (int i = 0; i < 4; i++) {
+        JsonToken arr_token = val.toArray()[i];
+        EXPECT_EQ_INT(JsonType::kArray, arr_token.type());
+        EXPECT_EQ_SIZE_T(i, arr_token.toArray().size());
+        for (int j = 0; j < i; j++) {
+            JsonToken num_token = arr_token.toArray()[j];
+            EXPECT_EQ_INT(JsonType::kNumber, num_token.type());
+            EXPECT_EQ_DOUBLE((double)j, num_token.toNumber());
+        }
+    };
 }
 
-static void test_parse_false() {
-    lept_value value{ lept_type::LEPT_FALSE, 0 };
-    lept_context context{ "false" };
-    EXPECT_EQ_INT(LEPT_PARSE_OK, context.lept_parse(value));
-    EXPECT_EQ_INT(lept_type::LEPT_FALSE, value.lept_get_type());
+static void testObject() {
+    Parser parser{ "{ }" }; 
+    JsonToken val = parser.parse();
+
+    EXPECT_EQ_INT(JsonType::kObject, val.type());
+    EXPECT_EQ_SIZE_T(0, val.toObject().size());
+
+    parser.setContext(
+        " { "
+        "\"n\" : null , "
+        "\"f\" : false , "
+        "\"t\" : true , "
+        "\"i\" : 123 , "
+        "\"s\" : \"abc\", "
+        "\"a\" : [ 1, 2, 3 ],"
+        "\"o\" : { \"1\" : 1, \"2\" : 2, \"3\" : 3 }"
+        " } "
+    );
+    val = parser.parse();
+    
+    EXPECT_EQ_INT(JsonType::kObject, val.type());
+    EXPECT_EQ_SIZE_T(7, val.toObject().size());
+    EXPECT_EQ_INT(JsonType::kNull, val["n"].type());
+    EXPECT_EQ_INT(JsonType::kBool, val["f"].type());
+    EXPECT_EQ_INT(JsonType::kBool, val["t"].type());
+    EXPECT_EQ_INT(JsonType::kNumber, val["i"].type());
+    EXPECT_EQ_DOUBLE(123.0, val["i"].toNumber());
+    EXPECT_EQ_INT(JsonType::kString, val["s"].type());
+    EXPECT_EQ_STRING("abc", val["s"].toString().c_str(), val["s"].toString().length());
+    EXPECT_EQ_INT(JsonType::kArray, val["a"].type());
+    EXPECT_EQ_SIZE_T(3, val["a"].size());
+    for(int i = 0; i < 3; i++){
+        EXPECT_EQ_INT(JsonType::kNumber, val["a"][i].type());
+        EXPECT_EQ_DOUBLE(i + 1.0, val["a"][i].toNumber());
+    }
+    EXPECT_EQ_INT(JsonType::kObject, val["o"].type());
+    string tmpKey = "";
+    char start = '1';
+    for (int i = 0; i < 3; i++){
+        tmpKey = "";
+        tmpKey += (start + i);
+        EXPECT_EQ_INT(JsonType::kNumber, val["o"][tmpKey].type());
+        EXPECT_EQ_DOUBLE(i + 1.0, val["o"][tmpKey].toNumber());
+    }
 }
-
-static void test_parse_number() {
-    TEST_NUMBER(0.0, "0");
-    TEST_NUMBER(0.0, "-0");
-    TEST_NUMBER(0.0, "-0.0");
-    TEST_NUMBER(1.0, "1");
-    TEST_NUMBER(-1.0, "-1");
-    TEST_NUMBER(1.5, "1.5");
-    TEST_NUMBER(-1.5, "-1.5");
-    TEST_NUMBER(3.1416, "3.1416");
-    TEST_NUMBER(1E10, "1E10");
-    TEST_NUMBER(1e10, "1e10");
-    TEST_NUMBER(1E+10, "1E+10");
-    TEST_NUMBER(1E-10, "1E-10");
-    TEST_NUMBER(-1E10, "-1E10");
-    TEST_NUMBER(-1e10, "-1e10");
-    TEST_NUMBER(-1E+10, "-1E+10");
-    TEST_NUMBER(-1E-10, "-1E-10");
-    TEST_NUMBER(1.234E+10, "1.234E+10");
-    TEST_NUMBER(1.234E-10, "1.234E-10");
-    TEST_NUMBER(0.0, "1e-10000000000000"); /* 下溢 */
-
-    TEST_NUMBER(1.0000000000000002, "1.0000000000000002");            /* 所有大于0的数中最小者 */
-    TEST_NUMBER(4.9406564584124654e-324, "4.9406564584124654e-324");  /* double类型最小非规格化浮点数 */
-    TEST_NUMBER(-4.9406564584124654e-324,  "-4.9406564584124654e-324");
-    TEST_NUMBER(2.2250738585072009e-308,  "2.2250738585072009e-308");  /* double类型的最大非规格化浮点数 */
-    TEST_NUMBER(-2.2250738585072009e-308,  "-2.2250738585072009e-308");
-    TEST_NUMBER(2.2250738585072014e-308,  "2.2250738585072014e-308");  /* 大于0的最小正规double类型 */
-    TEST_NUMBER(-2.2250738585072014e-308,  "-2.2250738585072014e-308");
-    TEST_NUMBER(1.7976931348623157e+308, "1.7976931348623157e+308");  /* double类型最大值 */
-    TEST_NUMBER(-1.7976931348623157e+308,  "-1.7976931348623157e+308");
-}
-
-static void test_parse_expect_value() {
-    TEST_ERROR(LEPT_PARSE_EXPECT_VALUE, "");
-    TEST_ERROR(LEPT_PARSE_EXPECT_VALUE, " ");
-}
-
-static void test_parse_invalid_value() {
-    TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "nul");
-    TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "?");
-
-    /* 非法数字 */
-    TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "+0");
-    TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "+1");
-    TEST_ERROR(LEPT_PARSE_INVALID_VALUE, ".123"); 
-    TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "1."); 
-    TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "INF");
-    TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "inf");
-    TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "NAN");
-    TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "nan");
-}
-
-static void test_parse_root_not_singular() {
-    TEST_ERROR(LEPT_PARSE_ROOT_NOT_SINGULAR, "null x");
-
-    TEST_ERROR(LEPT_PARSE_ROOT_NOT_SINGULAR, "0123"); 
-    TEST_ERROR(LEPT_PARSE_ROOT_NOT_SINGULAR, "0x0");
-    TEST_ERROR(LEPT_PARSE_ROOT_NOT_SINGULAR, "0x123");
-}
-
-static void test_parse_number_too_big() {
-    TEST_ERROR(LEPT_PARSE_NUMBER_TOO_BIG, "1e309");
-    TEST_ERROR(LEPT_PARSE_NUMBER_TOO_BIG, "-1e309");
-}
-
-static void test_parse() {
-    test_parse_null();
-    test_parse_expect_value();
-    test_parse_invalid_value();
-    test_parse_false();
-    test_parse_true();
-    test_parse_root_not_singular();
-    test_parse_number();
-    test_parse_number_too_big();
+static void testParse() {
+    testNull();
+    testFalse();
+    testTrue();
+    testNumber();
+    testString();
+    testArray();
+    testObject();
 }
 
 int main() {
-    test_parse();
+    testParse();
     printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
     return main_ret;
 }
